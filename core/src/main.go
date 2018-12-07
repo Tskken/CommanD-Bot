e132343e6e279890b9aacd88e1aa415f30b7b1ff
Tsukinai <dylan.blanchard@mymail.champlain.edu>
2018-12-07 08:05:23 -0500

    Starting from scratch
    every thing is gone!!!! we start from the ground up

---
+++ core/src/main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+CommanD-Bot Beta
+Author: Dylan Blanchard
+*/
+
+import "log"
+
+// Entry point //
+func main() {
+	log.Print("Project CC 1.0")
+
+	log.Fatal("Not Implemented!!!")
+}
--- interfaces.go
+++
@@ -1,5 +0,0 @@
-package CommanD_Bot
-
-type CommandFunction interface {
-	RunCommand(*Root)error
-}
